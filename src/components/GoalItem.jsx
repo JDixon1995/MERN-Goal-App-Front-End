2904a4d9e04c9ca34d2765ed7bc4f2dcf61f9366
John D <jddhalogamer@gmail.com>
2022-03-28 15:39:52 -0500

    removed junk code.

--- src/components/GoalItem.jsx
+++ src/components/GoalItem.jsx
@@ -1,21 +1,13 @@
 import { useDispatch } from 'react-redux'
-import { useState } from 'react'
 import { deleteGoal } from '../features/goals/goalSlice'
-import UpdateGoalPage from './UpdateGoalPage'
 
 
 function GoalItem({ goal }) {
 
 	const dispatch = useDispatch()
-	const [showUpdatePage, setShowUpdatePage] = useState(false)
 
   return (
 	<div className="goal">
-		<button 
-		onClick={() => {
-			console.log(goal._id)
-		}} 
-		className='btn btn-update'>Update</button>
 		<div className='goal-text'>
 		<h2>{goal.text}</h2>
 			{new Date(goal.createdAt).toLocaleString('en-US')}
